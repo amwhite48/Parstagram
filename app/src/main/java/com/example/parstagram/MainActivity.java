66a220f66fcd471cec39c566d541c52677d81093
Alana White <alanaw@fb.com>
2020-07-08 18:09:46 -0400

    added ability to take an image with the camera and post it

--- app/src/main/java/com/example/parstagram/MainActivity.java
+++ app/src/main/java/com/example/parstagram/MainActivity.java
@@ -1,17 +1,27 @@
 package com.example.parstagram;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "MainActivity";
     Button btLogout;
+    Button btPost;
+    RecyclerView rvPosts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btLogout = findViewById(R.id.btLogout);
+        btPost = findViewById(R.id.btPost);
+        rvPosts = findViewById(R.id.rvPosts);
 
         btLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +42,40 @@ public class MainActivity extends AppCompatActivity {
                 Intent i = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(i);
                 finish();
+            }
+        });
+
+        btPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // navigates to create post activity
+                Intent i = new Intent(MainActivity.this, PostActivity.class);
+                startActivity(i);
+
+            }
+        });
+        
+        queryPosts();
+    }
+
+    private void queryPosts() {
+        // Specify which class to query
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        // include user in query
+        query.include(Post.KEY_USER);
+        // find all posts
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                // if there are any errors, return
+                if(e != null) {
+                    Log.e(TAG, "issue with retrieving posts", e);
+                    return;
+                }
+                // iterate through all posts
+                for(Post post: posts) {
+                    Log.i(TAG, "Post: " + post.getDescription() + ", User: " + post.getUser().getUsername());
+                }
             }
         });
     }
---
+++ app/src/main/java/com/example/parstagram/PostActivity.java
@@ -0,0 +1,190 @@
+package com.example.parstagram;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class PostActivity extends AppCompatActivity {
+
+    public static final String TAG = "PostActivity";
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 48;
+    public static final int MAX_WIDTH = 800;
+    Button btTakePicture;
+    Button btPost;
+    ImageView ivToPost;
+    EditText etDescription;
+    private File photoFile;
+    public String photoFileName = "photo.jpg";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+
+        // find views
+        btTakePicture = findViewById(R.id.btTakePicture);
+        btPost = findViewById(R.id.btPost);
+        ivToPost = findViewById(R.id.ivToPost);
+        etDescription = findViewById(R.id.etDescription);
+
+        btPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String description = etDescription.getText().toString();
+                if(description.isEmpty()) {
+                    Toast.makeText(PostActivity.this, "Caption cannot be empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                // if no photo, don't post
+                if(photoFile == null || ivToPost.getDrawable() == null){
+                    Toast.makeText(PostActivity.this, "Error: no picture attached", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                //if post is valid, get user and create in Parse
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                savePost(description, currentUser, photoFile);
+
+            }
+        });
+
+        btTakePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // launch camera using implicit intent
+                launchCamera();
+            }
+        });
+    }
+
+    private void launchCamera() {
+        // create implicit  intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // create file reference for future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap photo File into a content provider to make sure image is stored properly
+        // required for API >= 24
+        Uri fileProvider = FileProvider.getUriForFile(PostActivity.this, "com.codepath.fileprovider", photoFile);
+        // tell application where you want output
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // checks if phone has camera
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    // return to post activity after taking a picture
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP,
+                try {
+                    Bitmap scaledImage = resize();
+                    // Load the taken image into a preview
+                    ivToPost.setImageBitmap(scaledImage);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private Bitmap resize() throws IOException {
+        Uri takenPhotoUri = Uri.fromFile(getPhotoFileUri(photoFileName));
+        // by this point we have the camera photo on disk
+        Bitmap rawTakenImage = BitmapFactory.decodeFile(takenPhotoUri.getPath());
+        // See BitmapScaler.java: https://gist.github.com/nesquena/3885707fd3773c09f1bb
+        Bitmap resizedBitmap = BitmapScaler.scaleToFitWidth(rawTakenImage, MAX_WIDTH);
+
+        // Configure byte output stream
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        // Compress the image further
+        resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 40, bytes);
+        // Create a new file for the resized bitmap (`getPhotoFileUri` defined above)
+        File resizedFile = getPhotoFileUri(photoFileName + "_resized");
+        resizedFile.createNewFile();
+        FileOutputStream fos = new FileOutputStream(resizedFile);
+        // Write the bytes of the bitmap to file
+        fos.write(bytes.toByteArray());
+        fos.close();
+
+        return resizedBitmap;
+    }
+
+    // helper to get photo file URI (string representing file)
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        return new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+    }
+
+    private void savePost(String description, ParseUser currentUser, final File photoFile) {
+        // create new post and set attributes
+        Post post = new Post();
+        post.setDescription(description);
+        post.setImage(new ParseFile(photoFile));
+        post.setUser(currentUser);
+
+        // save callback to save post to parse in background
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e != null) {
+                    // if an exception was thrown, there was an error
+                    Log.e(TAG, "Error while saving post", e);
+                    Toast.makeText(PostActivity.this, "Error saving post", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Log.i(TAG, "Posting successful!");
+                // clear edit text to give visual indication of successful post
+                etDescription.setText("");
+                ivToPost.setImageResource(0);
+            }
+        });
+    }
+}
